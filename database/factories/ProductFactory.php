9401943f41c91c967fec2ddd50513e3919b1e7e4
Ali Arefzadeh <poryaarmanfar@gmail.com>
2023-07-17 20:37:19 +0330

    step 4

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->name(),
+            'description' => $this->faker->realText($maxNbChars = 200, $indexSize = 2),
+            'price' => rand(450000,3000000),
+            'availability' => rand(0, 1),
+
+        ];
+    }
+}
