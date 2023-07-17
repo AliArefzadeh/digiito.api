c2038199a27356c8c27a83b071e24afbff5c1fda
Ali Arefzadeh <poryaarmanfar@gmail.com>
2023-07-17 20:36:30 +0330

    step 4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,23 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('products', function () {
-    return response(['Product 1', 'Product 2', 'Product 3'],200);
-});
-Route::get('products/{product}', function ($productId) {
-    return response()->json(['productId' => "{$productId}"], 200);
-});
 
-Route::post('products', function() {
-    return  response()->json([
-        'message' => 'Create success'
-    ], 201);
-});
-Route::put('products/{product}', function() {
-    return  response()->json([
-        'message' => 'Update success'
-    ], 200);
-});
-Route::delete('products/{product}',function() {
-    return  response()->json(null, 204);
-});
+Route::get('products', [ProductController::class,'index']);
+Route::get('products/{product}', [ProductController::class,'show']);
+Route::post('products',[ProductController::class,'store']);
+Route::put('products/{product}',[ProductController::class,'update']);
+Route::delete('products/{product}', [ProductController::class,'delete']);
+
