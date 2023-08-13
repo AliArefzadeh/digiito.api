84323525f65da24570d3b0c4eb0753620ca6e01a
Ali Arefzadeh <poryaarmanfar@gmail.com>
2023-08-13 11:42:11 +0330

    test 3

--- database/migrations/2023_08_01_093029_create_products_table.php
+++ database/migrations/2023_08_01_093029_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('category_id')->constrained();
+            $table->string('category_id');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
